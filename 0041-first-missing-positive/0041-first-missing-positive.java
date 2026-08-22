9534b31383587cdd576bdd556e7e39d7819421e3
sanket8459 <sanketsangle89@gmail.com>
2026-08-22 19:33:30 +0530

    Time: 18 ms (13.14%), Space: 74 MB (26.22%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int smallest = 1;
+
+        for (int num : nums) {
+
+            if (num == smallest) {
+                smallest++;
+            }
+        }
+
+        return smallest;
+    }
+}
